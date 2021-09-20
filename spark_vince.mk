f1a61aa9291b3497ae7a46d3b6297712602a855b
ANIMESH SINGH <me.animesh22@gmail.com>
2021-09-20 12:41:15 +0530

    Vince: remove unwanted stuff

--- spark_vince.mk
+++ spark_vince.mk
@@ -32,10 +32,6 @@ $(call inherit-product, vendor/spark/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1440
 TARGET_DISABLE_POSTRENDER_CLEANUP := true
 
-# Maintainer
-SPARK_BUILD_TYPE := OFFICIAL
-PRODUCT_PRODUCT_PROPERTIES += \
-   ro.spark.maintainer=ENTER_NAME \
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := vince
